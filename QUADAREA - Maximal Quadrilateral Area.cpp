d157bab13386deac81d0681e73531cf24df2fb60
alexka28 <horvath.alex28@gmail.com>
2018-02-05 17:45:30 +0100

    Maximal Quadrilateral Area with Brahmagupta's formula

---
+++ QUADAREA - Maximal Quadrilateral Area.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <stdio.h>
+//QUADAREA - Maximal Quadrilateral Area
+//Brahmagupta's formula
+using namespace std;
+double area(double a,double b,double c,double d)
+{
+    double s = (a+b+c+d)/2;
+    double k;
+    k = sqrt((s-a)*(s-b)*(s-c)*(s-d));
+    return k;
+
+}
+int main()
+{
+
+    int t,i=0;
+    double a,b,c,d;
+    cin>>t;
+    while(i != t)
+    {
+        cin >>a>>b>>c>>d;
+
+        printf("%.2lf ", area(a,b,c,d));
+
+
+        ++i;
+    }
+
+
+    return 0;
+}
